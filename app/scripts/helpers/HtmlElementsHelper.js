0043d45855d9721abc85c1ec3fc26152191232dd
Lucas Cavalcante <lucascavalcante@gmail.com>
2017-05-21 22:30:34 -0300

    view quiz finished

--- app/scripts/helpers/HtmlElementsHelper.js
+++ app/scripts/helpers/HtmlElementsHelper.js
@@ -136,7 +136,7 @@ class HtmlElementsHelper {
                 let txt = document.createTextNode(nestedData[i].questions[y].title);
                 div.appendChild(txt);
 
-                let input = this.inputType(nestedData[i].questions[y].answers[0].answerType);
+                let input = this.inputType(nestedData[i].questions[y].answers[0].answerType, nestedData[i].questions[y].answers[0].optionsAnswer, nestedData[i].questions[y].id);
                 div.appendChild(input);
 
                 fieldset.appendChild(div);
@@ -145,17 +145,139 @@ class HtmlElementsHelper {
             form.appendChild(fieldset);
         }
 
+        let button = document.createElement('button');
+        button.classList.add('btn');
+        button.classList.add('btn-primary');
+        button.classList.add('form-control');
+
+        let txtButon = document.createTextNode('Finalizar Questionário');
+        button.appendChild(txtButon);
+
+        form.appendChild(button);
+
         document.querySelector('.modal-body').insertBefore(form, null);
     }
 
-    static inputType(answerType) {
-        var input = null;
+    static inputType(answerType, optionsAnswer, questionId) {
         if(answerType === 'shortText') {
-            input = document.createElement('input');
+            let input = document.createElement('input');
             input.setAttribute('type', 'text');
             input.classList.add('form-control');
+            return input;
+
+        } else if(answerType === 'longText') {
+            let textarea = document.createElement('textarea');
+            textarea.classList.add('form-control');
+            return textarea;
+
+        } else if(answerType === 'date') {
+            let input = document.createElement('input');
+            input.setAttribute('type', 'date');
+            input.classList.add('form-control');
+            return input;
+
+        } else if(answerType === 'datetime') {
+            let input = document.createElement('input');
+            input.setAttribute('type', 'datetime-local');
+            input.classList.add('form-control');
+            return input;
+
+        } else if(answerType === 'radio') {
+            let div = document.createElement('div');
+
+            let options = optionsAnswer.split(',');
+
+            for(let i = 0; i < options.length; i++) {
+                let input = document.createElement('input');
+                input.setAttribute('type', 'radio');
+                input.setAttribute('name', 'question-' + questionId);
+                div.appendChild(input);
+
+                let label  = document.createElement('label');
+                label.setAttribute('for', 'question-' + questionId);
+                let labelText = document.createTextNode(options[i]);
+                label.appendChild(labelText);
+                div.appendChild(label);
+
+                let br = document.createElement('br');
+                div.appendChild(br);
+                
+            }
+
+            return div;
+
+        } else if(answerType === 'checkbox') {
+            let div = document.createElement('div');
+
+            let options = optionsAnswer.split(',');
+
+            for(let i = 0; i < options.length; i++) {
+                let input = document.createElement('input');
+                input.setAttribute('type', 'checkbox');
+                input.setAttribute('name', 'question-' + questionId);
+                div.appendChild(input);
+
+                let label  = document.createElement('label');
+                label.setAttribute('for', 'question-' + questionId);
+                let labelText = document.createTextNode(options[i]);
+                label.appendChild(labelText);
+                div.appendChild(label);
+
+                let br = document.createElement('br');
+                div.appendChild(br);
+                
+            }
+
+            return div;
+
+        } else if(answerType === 'number') {
+            let input = document.createElement('input');
+            input.setAttribute('type', 'number');
+            input.classList.add('form-control');
+            return input;
+
+        } else if(answerType === 'boolean') {
+            let select = document.createElement('select');
+            select.classList.add('form-control');
+
+            let optionTrue = document.createElement('option');
+            optionTrue.value = '1';
+            optionTrue.text = 'Sim';
+            select.appendChild(optionTrue);
+
+            let optionFalse = document.createElement('option');
+            optionFalse.value = '0';
+            optionFalse.text = 'Não';
+            select.appendChild(optionFalse);
+
+            return select;
+
+        } else if(answerType === 'rating') {
+            let div = document.createElement('div');
+            div.classList.add('stars');
+
+            for(let i = 5; i >= 1; i-- ) {
+
+                let input = document.createElement('input');
+                input.classList.add('star');
+                input.classList.add('star-' + i);
+                input.setAttribute('id', 'star-' + i);
+                input.setAttribute('type', 'radio');
+                input.setAttribute('name', 'star');
+                div.appendChild(input);
+
+                let label = document.createElement('label');
+                label.classList.add('star');
+                label.classList.add('star-' + i);
+                label.setAttribute('for', 'star-' + i);
+                div.appendChild(label);
+                
+            }
+
+
+            return div;
+
         }
-        return input;
     }
     
 }
